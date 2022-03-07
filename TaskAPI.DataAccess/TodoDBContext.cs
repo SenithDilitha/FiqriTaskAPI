227646ae0838a2c242160a76fbdd84fd2377fe8e
Senith Dilitha <senith@excelly.io>
2022-03-07 15:09:43 +0530

    Working state step 5

---
+++ TaskAPI.DataAccess/TodoDBContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskAPI.Models;
+
+namespace TaskAPI.DataAccess
+{
+    public class TodoDBContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost; Database=MyTodoDB; User Id=sa2; Password=Welcome123";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(new Todo
+            {
+                Id = 3,
+                Title = "Get books for school from DB",
+                Description = "Get some text books for school DB",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(2),
+                Status = TodoStatus.New
+            });
+        }
+    }
+
+}
